78b0f9062c064a46c839454246eb9bb041038bee
iMADi-ARCH <nandanaditya985@gmail.com>
2023-11-26 12:11:38 +0530

    feat: dark mode

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,6 +1,8 @@
 "use client";
+import dynamic from "next/dynamic";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+const ThemeProvider = dynamic(() => import("./theme-provider"), { ssr: false });
 
 // Create a client
 const queryClient = new QueryClient();
@@ -11,7 +13,16 @@ type Props = {
 
 const Providers = ({ children }: Props) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        defaultTheme="system"
+        attribute="class"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
--- components/sections/chapters/Chapters.tsx
+++ components/sections/chapters/Chapters.tsx
@@ -24,7 +24,7 @@ const Chapters = (props: Props) => {
   });
   // const chapters = await getChapters(supabase);
   if (isError) {
-    return error;
+    return null;
   }
 
   return (
